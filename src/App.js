45e6f0521dfd46ca6a46558dd0bd1008e1ccc2d8
Mario Alberto Avila Rehlaender <e-maavila@neoris.cxnetworks.net>
2017-09-27 18:32:10 -0500

    Made a lel

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Script from 'react-load-script'
+import tableau from 'tableau-api';
 class App extends Component {
+  constructor(props) {
+     super(props);
+     this.state = { tableauSrc: 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms' };
+   }
 
   componentWillMount() {
-    alert('will mount');
 
+    const script = document.createElement("script");
+
+    script.src = "https://public.tableau.com/javascripts/api/tableau-2.min.js";
+    script.async = true;
+
+    document.body.appendChild(script);
   }
 
   componentDidMount() {
-    alert('did mount');
-
-
-
+    this.initViz();
   }
 
-  // initViz() {
-  //   var containerDiv = document.getElementById("vizContainer"),
-  //       url = "http://public.tableau.com/views/RegionalSampleWorkbook/Storms",
-  //       options = {
-  //         hideTabs: true,
-  //         onFirstInteractive: function () {
-  //           console.log("Run this code when the viz has finished loading.");
-  //         }
-  //       };
-  //       // Create a viz object and embed it in the container div.
-  //       var viz = new tableau.Viz(containerDiv, url, options);
-  // }
+  initViz() {
+    const containerDiv = document.getElementById("containerDiv");
+    const options = {
+          hideTabs: true,
+          hideToolbar: true,
+          onFirstInteractive: function () {
+            console.log("Run this code when the viz has finished loading.");
+          }
+        };
+    let viz = new window.tableau.Viz(containerDiv, this.state.tableauSrc, options);
+    console.log(viz);
+  }
+
+  consolero() {
+    alert("lel");
+    console.log('Lel');
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div className="containerDiv"></div>
+        {this.state.tableauSrc}
+        <button onClick={this.consolero.bind(this)}>Click</button>
+        <div id="containerDiv"></div>
       </div>
     );
   }
