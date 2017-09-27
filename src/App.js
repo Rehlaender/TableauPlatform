a35beb5b0958911575bd08b12feacff9adb58210
Mario Alberto Avila Rehlaender <e-maavila@neoris.cxnetworks.net>
2017-09-27 16:14:45 -0500

    Make the shitty way

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentWillMount() {
+    alert('will mount');
+
+  }
+
+  componentDidMount() {
+    alert('did mount');
+
+
+
+  }
+
+  // initViz() {
+  //   var containerDiv = document.getElementById("vizContainer"),
+  //       url = "http://public.tableau.com/views/RegionalSampleWorkbook/Storms",
+  //       options = {
+  //         hideTabs: true,
+  //         onFirstInteractive: function () {
+  //           console.log("Run this code when the viz has finished loading.");
+  //         }
+  //       };
+  //       // Create a viz object and embed it in the container div.
+  //       var viz = new tableau.Viz(containerDiv, url, options);
+  // }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +39,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="containerDiv"></div>
       </div>
     );
   }
