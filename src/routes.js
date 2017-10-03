fd36c559351e04a37053b6a077f2e12bf157f5b5
Mario Avila <avilamario16@gmail.com>
2017-10-03 18:41:52 -0500

    Add static object

---
+++ src/routes.js
@@ -0,0 +1,114 @@
+const Routes = [
+  {
+    'title': 'Manufactura',
+    'context': [
+      {
+        'title': 'Desarrollo',
+        'context': [
+          {
+            'title': 'La 1',
+            'context': [
+              {
+                'title': 'La 1 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              },
+              {
+                'title': 'La 2 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              },
+              {
+                'title': 'La 3 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              }
+            ]
+          }
+        ]
+      },
+      {
+        'title': 'Not desarrolloDesarrollo',
+        'context': [
+          {
+            'title': 'La 1',
+            'context': [
+              {
+                'title': 'La 1 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              },
+              {
+                'title': 'La 2 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              },
+              {
+                'title': 'La 3 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    'title': 'Not Manufactura',
+    'context': [
+      {
+        'title': 'Desarrollo',
+        'context': [
+          {
+            'title': 'La 1',
+            'context': [
+              {
+                'title': 'La 1 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              },
+              {
+                'title': 'La 2 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              },
+              {
+                'title': 'La 3 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              }
+            ]
+          }
+        ]
+      },
+      {
+        'title': 'Not desarrolloDesarrollo',
+        'context': [
+          {
+            'title': 'La 1',
+            'context': [
+              {
+                'title': 'La 1 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              },
+              {
+                'title': 'La 2 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              },
+              {
+                'title': 'La 3 children',
+                'icon': '',
+                'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
+
+export default Routes;
