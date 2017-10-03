0d02de7ec2dcdbbac76a2ece783c878c1eef77f9
Mario Avila <avilamario16@gmail.com>
2017-10-03 18:47:37 -0500

    Add routes ids

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,30 @@
 const Routes = [
   {
+    'id': 'manufactura',
     'title': 'Manufactura',
     'context': [
       {
+        'id': 'notdesarrollo',
         'title': 'Desarrollo',
         'context': [
           {
+            'id': 'la1',
             'title': 'La 1',
             'context': [
               {
+                'id': 'la1children',
                 'title': 'La 1 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
               },
               {
+                'id': 'la2children',
                 'title': 'La 2 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
               },
               {
+                'id': 'la3children',
                 'title': 'La 3 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
@@ -28,22 +34,27 @@ const Routes = [
         ]
       },
       {
+        'id': 'notdesarrollo',
         'title': 'Not desarrolloDesarrollo',
         'context': [
           {
+            'id': 'la1',
             'title': 'La 1',
             'context': [
               {
+                'id': 'la1children',
                 'title': 'La 1 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
               },
               {
+                'id': 'la2children',
                 'title': 'La 2 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
               },
               {
+                'id': 'la3children',
                 'title': 'La 3 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
@@ -55,25 +66,31 @@ const Routes = [
     ]
   },
   {
+    'id': 'notmanufactura',
     'title': 'Not Manufactura',
     'context': [
       {
+        'id': 'notdesarrollo',
         'title': 'Desarrollo',
         'context': [
           {
+            'id': 'la1',
             'title': 'La 1',
             'context': [
               {
+                'id': 'la1children',
                 'title': 'La 1 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
               },
               {
+                'id': 'la2children',
                 'title': 'La 2 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
               },
               {
+                'id': 'la3children',
                 'title': 'La 3 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
@@ -83,22 +100,27 @@ const Routes = [
         ]
       },
       {
+        'id': 'notdesarrollo',
         'title': 'Not desarrolloDesarrollo',
         'context': [
           {
+            'id': 'la1',
             'title': 'La 1',
             'context': [
               {
+                'id': 'la1children',
                 'title': 'La 1 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
               },
               {
+                'id': 'la2children',
                 'title': 'La 2 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
               },
               {
+                'id': 'la3children',
                 'title': 'La 3 children',
                 'icon': '',
                 'tableauUrl': 'http://public.tableau.com/views/RegionalSampleWorkbook/Storms',
