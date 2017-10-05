c241d0726191dd625399b1f8a00806bc6910975b
Mario Alberto Avila Rehlaender <e-maavila@neoris.cxnetworks.net>
2017-10-05 15:42:52 -0500

    Add entrance drawer

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -6,19 +6,33 @@ import './Drawer.css';
 import Routes from '../../routes.js';
 
 class Drawer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      overMenuBottom: '-100vh',
+      overMenuDisplay: 'none'
+    };
+  }
 
-  goTo(route) {
-    console.log('- well call this object');
+  toggleOverMenu() {
+    if(this.state.overMenuBottom === '-100vh') {
+      this.setState({overMenuDisplay: 'flex'});
+      setTimeout( ()=> {this.setState({overMenuBottom: '-40px'})}, 100);
+
+    } else {
+      this.setState({overMenuBottom: '-100vh'});
+      setTimeout( ()=> {this.setState({overMenuDisplay: 'none'})}, 550);
+    }
   }
 
   render() {
     return (
       <div id="drawer" className={["flex flex-column"]}>
-        <div className={["over-menu"]}></div>
+        <div style={{top: this.state.overMenuBottom, display: this.state.overMenuDisplay }} className={["over-menu"]}></div>
         <div id="action-bar" className={["flex flex-row flex-jc-space-around flex-ai-center"]}>
           <div onClick={ ()=>{this.props.goBack();} } className={["back icon"]}>Go Back</div>
           <div className={["home icon"]}>HOME</div>
-          <div className={["burger icon"]}>Burger</div>
+          <div onClick={this.toggleOverMenu.bind(this)} className={["burger icon"]}>Burger</div>
         </div>
       </div>
     );
