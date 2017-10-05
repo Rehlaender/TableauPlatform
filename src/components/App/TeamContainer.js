be1d7cb6c24604fde19a950217db45d8e91f6500
Mario Alberto Avila Rehlaender <e-maavila@neoris.cxnetworks.net>
2017-10-05 13:58:12 -0500

    Style the TeamContainer

--- src/components/App/TeamContainer.js
+++ src/components/App/TeamContainer.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Flexbox } from 'flexbox-react';
 
 import '../../flexbox.css';
-
+import './TeamContainer.css';
 
 import Routes from '../../routes.js';
 
@@ -39,30 +38,40 @@ class TeamContainer extends Component {
 
     return (
       <div className={["team-container flex flex-column flex-all-center"]}>
-        <div className={["title"]}>
+        <div className={["team-title flex flex-row flex-all-center"]}>
           This is team { Team.title } container
         </div>
+        <div className={["team-content flex flex-row flex-wrap flex-jc-space-around"]}>
         {
           // Sections containers
           Team.context.map((Section, i) =>
-            <div key={i} className={["section-container"]}>
-              <div className={["section-title"]}>{Section.title}</div>
+            <div key={i}
+              className={["section-container flex flex-column"]}>
+              <div className={["section-title"]}>
+                {Section.title}
+              </div>
+              <div className={["section-content flex flex-row flex-jc-space-between flex-wrap"]}>
               {
                 // Visualizations
                 Section.context.map((Visualization, j) =>
                   <Link key={j}
                         to={'/visualization/' + Team.id +
                             '/' + Section.id + '/' + Visualization.id  }
-                        className={["visualization-container"]}    >
-                    <div>
+                        className={["visualization-container flex flex-column flex-all-center"]} >
+                    <div className={["visualization-icon flex flex-all-center"]}>
+                     //
+                    </div>
+                    <div className={["visualization-title"]}>
                       {Visualization.title}
                     </div>
                   </Link>
                 )
               }
+              </div>
            </div>
           )
         }
+        </div>
       </div>
     );
   }
